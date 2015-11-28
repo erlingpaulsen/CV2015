1c28dc0efc2bc617fe1e48db8a1176a7cd7f6d18
Lars <larslb@stud.ntnu.no>
2015-11-28 16:07:13 +0100

    DepthNormalization function added, removed unsusable file -lb

--- DepthNormalization.m
+++ DepthNormalization.m
@@ -1,11 +1,17 @@
+function J = DepthNormalization(I_orig)
 %% Depth image normalization
-clear
-I_orig = imread('rgbd-dataset/apple/apple_1/apple_1_1_1_depthcrop.png');
+% 
+% Author: Lars Loeken Bjoernskau and Erling Singstad Paulsen
+%
+% Depth image function that produces a new image depth normalized by
+% original depth image I
+% J = DepthNormalization(I)
+
+% depth = depthnormalization(I)
 I = I_orig;
 C = 0; % Counts pixels removed each iteration
 [row, col] = size(I); % Size of picture
 K = (row-2)*(col-2); % Columns in im2col
-subit = 1; % Desiced which subiteration to use
 stop = false; % False while thinning is possible
 w = [3, 3]; % Mask size
 
@@ -17,35 +23,44 @@ w = [3, 3]; % Mask size
 while (~stop)
     Icol = im2col(I, w);
     M = zeros(1, K); % To store pixels that should be removed
-    
-    if subit == 1
-        % First subiteration condition
-        c = Icol(4, :).*Icol(8, :).*Icol(6, :);
-        d = Icol(8, :).*Icol(6, :).*Icol(2, :);
-    else
-        % Second subiteration condition
-        c = Icol(4, :).*Icol(8, :).*Icol(2, :);
-        d = Icol(4, :).*Icol(6, :).*Icol(2, :);
-    end
+   
     
     P1 = Icol(5, :); % The pixel we are looking at 
-    B = sum(Icol([1:4, 6:9], :)); % The sum of neighbouring pixels of P1
-    A = zeros(1, K); % Number of 01 patterns clockwise around each pixel
+    B = max(Icol([1:4, 6:9], :)); % The sum of neighbouring pixels of P1
     
     for i = 1 : K
         % Checking if we are on countour and if a), c) and d) holds
         if P1(i)==0 
-            M(i) = B/9;
+            M(i) = B(i);
             C = C + 1;
         end
     end
     
-    I2 = col2im(P1+M, w, [row, col]); % Removing pixel
+    
+    
+    I2 = col2im(double(P1)+M, w, [row, col]); % Removing pixel
     I(2:row-1, 2:col-1) = I2;
+    
+    %clearing borders
+    for i=1:row:row
+        for j=1:col-1
+            if I(i,j) == 0
+               I(i,j) = I(i,j-1);
+            end
+        end
+    end
+    for i=1:col:col
+        for j=1:row-1
+            if I(j,i) == 0
+               I(j,i) = I(j-1,i);
+            end
+        end
+    end
     
     stop = C == 0; % If no pixels are removed, the loop ends
     C = 0;
-    subit = mod(subit, 2) + 1; % Alternating between subiteration 1 and 2
 end
 
-imshow(I2);
+J = I;
+
+
